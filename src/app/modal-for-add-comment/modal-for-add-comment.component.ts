f314f312913f8dd75412edb10f4242e5ac3d6417
MarinkaPola <marinkamandarinca@gmail.com>
2020-07-08 17:55:09 +0300

    bez obnovlenia1

---
+++ src/app/modal-for-add-comment/modal-for-add-comment.component.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import {ChatService, Message} from '../comments/chat.service';
+import {Subscription} from 'rxjs';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+
+
+@Component({
+  selector: 'app-modal-for-add-comment',
+  templateUrl: './modal-for-add-comment.component.html',
+  styleUrls: ['./modal-for-add-comment.component.scss']
+})
+export class ModalForAddCommentComponent implements OnInit {
+  private subscription: Subscription;
+  id: string;
+  idmes: string;
+  textmes: string;
+  namemes: string;
+  datemes: Date;
+  vartruefalse = true;
+  text: string;
+  private message: Message;
+  private form: FormGroup;
+
+  constructor(private chatServise: ChatService) {
+  }
+  messages: Message[] = [];
+  ngOnInit() {
+    this.subscription = this.chatServise.getMessageEdit().subscribe(data => {
+      if (data.event === 'openModalWithComponent') {
+        this.showModal();
+      }
+      this.id = data.id;
+      this.idmes = data.idmes;
+      this.textmes = data.textmes;
+      this.datemes = data.datemes;
+      this.namemes = data.namemes;
+      console.log(this.textmes);
+      console.log(this.namemes);
+      console.log(this.datemes);
+      console.log(this.idmes);
+      console.log(this.id);
+    });
+  }
+
+  showModal() {
+    this.vartruefalse = !this.vartruefalse;
+  }
+
+  saveNametext(textmes: string, namemes: string, idmes: string, datemes: Date, id: string) {
+    if (this.text !== undefined) {
+      this.chatServise.saveNametext({idmes: this.idmes, textmes: this.textmes, datemes: this.datemes, namemes: this.namemes}, this.id)
+          .subscribe(() => {
+            //
+            this.message.textmes = this.textmes;
+            this.message.idmes = this.idmes;
+            this.message.datemes = this.datemes;
+            this.message.namemes = this.namemes;
+            });
+          }
+    this.text = '';
+    this.showModal();
+    }
+  }
+
